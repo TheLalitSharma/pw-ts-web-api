05e013c9180b57ba610f9fddfed144f020a2c1ba
TheLalitSharma <connect.lalitsharma@gmail.com>
2025-11-07 18:14:02 +0530

    Added GitHub Env variable for retry

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -2,6 +2,7 @@ import base from './playwright.config';
 
 export default{
     ...base,
+    retries: Number(process.env.PW_RETRIES ?? 2),
     reporter: [['line'], ['html'], ['allure-playwright']],
     use: { ...base.use, trace: 'retain-on-failure', launchOptions: { slowMo: 1000} },
 }
