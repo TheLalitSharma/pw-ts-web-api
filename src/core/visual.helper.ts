c83398d416097446f49802a4c9d556b4b30d5189
TheLalitSharma <connect.lalitsharma@gmail.com>
2025-11-07 16:14:56 +0530

    Relaxed visual comparison

--- src/core/visual.helper.ts
+++ src/core/visual.helper.ts
@@ -3,6 +3,6 @@ import { Locator, Page, expect } from '@playwright/test';
 export async function expectPageScreenshot(page: Page, name: string, masks?: Locator[]) {
   await expect(page).toHaveScreenshot(`${name}.png`, {
     mask: masks,
-    maxDiffPixelRatio: 0.02
+    maxDiffPixelRatio: 0.20
   });
 }
