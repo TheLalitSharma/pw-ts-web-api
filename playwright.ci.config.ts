07ed3ffa40241ddb07624d4f068e98f8b9eb6cf5
TheLalitSharma <connect.lalitsharma@gmail.com>
2025-11-07 16:00:14 +0530

    Added slowmo in CI run

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -3,5 +3,5 @@ import base from './playwright.config';
 export default{
     ...base,
     reporter: [['line'], ['html'], ['allure-playwright']],
-    use: { ...base.use, trace: 'retain-on-failure' },
+    use: { ...base.use, trace: 'retain-on-failure', launchOptions: { slowMo: 1000} },
 }
